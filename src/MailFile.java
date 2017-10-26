92151b5734d78f5ff00080b2ad12462cba2cf70d
Punneliparambil <Agin.Punneliparambil@haw-hamburg.de>
2017-10-26 15:05:00 +0200

    first commit

---
+++ src/MailFile.java
@@ -0,0 +1,65 @@
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class MailFile{
+
+    private static final Logger LOG = Logger.getLogger(MailFile.class.getName());
+
+    private SSLSocketFactory sslSocketFactory;
+    private SSLSocket sslSocket;
+    private DataOutputStream dataOutputStream;
+    
+
+
+    private String receiverEmail;
+    private String attachment;
+
+
+    //https://www.programcreek.com/java-api-examples/javax.net.ssl.SSLSocketFactory
+    public MailFile(String host, Integer port) throws IOException {
+        configLogger();
+        sslSocketFactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+        try{
+            sslSocket = (SSLSocket) sslSocketFactory.createSocket(host,port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void main(String[] args){
+
+    }
+
+
+    public void handshake(){
+
+    }
+
+    private static void configLogger() {
+        try {
+            Handler handler = new FileHandler("log.txt");
+
+            SimpleFormatter formatter = new SimpleFormatter();
+            handler.setFormatter(formatter);
+
+            LOG.addHandler(handler);
+
+            LOG.info("HALLO0");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
